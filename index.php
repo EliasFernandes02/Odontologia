477d5aa2cb0e275bb3922f39943cbb1643f5924f
Elias Fernandes <eliasneto0014@gmail.com>
2022-07-01 14:52:58 -0300

    commit 1

---
+++ index.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Odonto</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="css/style.css"> 
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper@8/swiper-bundle.min.css"/>
+    
+</head>
+<body>
+
+<header class=" header ">
+
+
+            <a href="#" class="logo">DENTAL <span>CARE.</span></a>
+
+            <nav class="nav">
+                <a href="#home">home</a>
+                <a href="#about">sobre</a>
+                <a href="#services">serviços</a>
+                <a href="#review">reviews</a>
+                <a href="#contact">contatos</a>
+
+            </nav>
+
+            <a href="#contact" class="link-btn">Marque sua Consulta</a>
+
+            <div id="menu-btn" class="fas fa-bars"></div>
+
+</header>
+
+
+<section class="home" id="home">
+
+    <div class="container">
+        <div class="row min-vh-100 align-items-center">
+            <div class="content ">
+                <h3>Venha melhorar seu sorriso</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi dignissimos hic optio quod expedita et doloribus eius magnam velit vitae?</p>
+                <a href="#contact" class="link-btn">Marque sua sessão</a>
+
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+
+<section class="about" id="about">
+
+    <div class="container">
+
+        <div class="row align-items-center">
+
+        <div class="col-md-6 image">
+            <img src="midia/trabalho1.jfif" class="w-100 mb-4 mb-md-0"alt="">
+
+        </div>
+
+        <div class="col-md-6 content">
+            <span>Sobre Nós</span>
+            <h3>Tratamento para família</h3>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus fugit eveniet aut! Voluptatem provident accusamus repellat eligendi? Ex, ut doloremque!</p>
+            <a href="contact" class="link-btn">Marque sua consulta</a>
+
+        </div>
+
+        </div>
+    </div>
+
+</section>
+
+<section id="services" class="services">
+
+    <h1 class="heading">Nossos Serviços</h1>
+
+    <div class="box-container container">
+
+        <div class="box">
+            <img src="midia/icone1.png" alt="">
+            <h3>Limpeza</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, iusto?</p>
+
+        </div>
+
+        <div class="box">
+            <img src="midia/icone2.png" alt="">
+            <h3>Alinhamento Dentário</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, iusto?</p>
+
+        </div>
+
+        <div class="box">
+            <img src="midia/icone5.png" alt="">
+            <h3>Remoção de tártaro</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, iusto?</p>
+
+        </div>
+
+        <div class="box">
+            <img src="midia/icone3.png" alt="">
+            <h3>Canal</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, iusto?</p>
+
+        </div>
+
+        <div class="box">
+            <img src="midia/icone4.png" alt="">
+            <h3>Preenchimento com Resina</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, iusto?</p>
+
+        </div>
+
+        <div class="box">
+            <img src="midia/icone6.png" alt="">
+            <h3>fluor</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita, iusto?</p>
+
+        </div>
+
+    </div>
+
+</section>
+
+<section class="process" id="process">
+    
+    <h1 class="heading">Processos de Trabalho</h1>
+
+    <div class="box-container container">
+
+        <div class="box">
+            <img src="midia/procedure1.png" alt="">
+            <h3>Alinhamento</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, vero.</p>
+
+        </div>
+
+        <div class="box">
+            <img src="midia/procedure2.png" alt="">
+            <h3>Canal</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, vero.</p>
+
+        </div>
+
+
+        <div class="box">
+            <img src="midia/procedure4.png" alt="">
+            <h3>Limpeza</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi, vero.</p>
+
+        </div>
+
+
+     
+
+
+    </div>
+
+</section>
+
+
+<section class="review" id="review">
+
+    <h1 class="heading">Reviews</h1>
+        
+        <div class="box-container container">
+            <div class="box">
+                <img src="midia/review1.jpg">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, ducimus.</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+
+                </div>
+                <h3>Choquinha</h3>
+                <span>cliente satisfeito</span>
+
+            </div>
+
+            <div class="box">
+                <img src="midia/review2.jfif">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, ducimus.</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+
+                </div>
+                <h3>Chiquim</h3>
+                <span>cliente satisfeito</span>
+
+            </div>
+
+            <div class="box">
+                <img src="midia/review3.jfif">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas, ducimus.</p>
+                <div class="stars">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star-half-alt"></i>
+
+                </div>
+                <h3>Jãozim</h3>
+                <span>cliente satisfeito</span>
+
+            </div>
+
+
+
+        </div>
+
+
+</section>
+
+
+
+<section class="contact" id="contact">
+
+    <h1 class="heading"> Marque sua consulta       </h1>
+        <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+        <?php
+            if(isset($message)){
+                echo '<p class="message">'.$message.'</p>';
+            }
+        ?>
+
+        
+        <span>Seu nome</span>
+        <input type="text" name="name" placeholder="Digite seu nome" class="box">
+        <span>Seu email</span>
+        <input type="email" name="email" placeholder="Digite seu email" class="box">
+        <span>Seu numero</span>
+        <input type="number" name="number" placeholder="Digite seu numero" class="box">
+        <span>Dia de atendimento</span>
+        <input type="datetime-local" name="date" class="box">
+        <input type="submit" value="Marque sua consulta" name="submit" class="link-btn">
+    
+        </form>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<script src="javascript/javascript.js"></script>
+<script src="https://kit.fontawesome.com/52b13b8b2d.js" crossorigin="anonymous"></script>
+</body>
+</html>
